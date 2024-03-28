8b00e4686503d6012593a977646f0b392d2d8389
Quan Nguyen <nguyen.quan1818@yahoo.com>
2024-03-28 00:59:49 -0400

    removed these files

--- script.js
+++
@@ -1,9 +0,0 @@
-const entryForm = document.getElementById("entry-form");
-const doneDialog = document.getElementById("doneDialog");
-const returnHomeBtn = document.getElementById("returnHome");
-
-entryForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    doneDialog.showModal();
-})
-
